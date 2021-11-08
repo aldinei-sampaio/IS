d41d6ac9fb0fa778e4dccbb07d097d9f07e25030
Aldinei Sampaio <aldinei.sampaio@pgmais.com.br>
2021-11-07 23:35:50 -0300

    #20 - Criado modelo visual e iniciando implementação em Blazor

---
+++ src/IS.Blazor/Client/Services/InternalAssetManager.cs
@@ -0,0 +1,41 @@
+﻿using IS.Blazor.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace IS.Blazor.Services
+{
+    public class InternalAssetManager : IAssetManager
+    {
+        private readonly HttpClient httpClient;
+
+        public InternalAssetManager(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<IReadOnlyList<BookModel>> GetBooksAsync()
+            => await httpClient.GetFromJsonAsync<BookModel[]>($"data/books/books.json?{DateTime.Now:yyyyMMddHHmmss}");
+
+        public string GetBookCoverUrl(string bookName)
+            => $"data/books/{bookName}/cover.jpg";
+
+        public async Task<BookModel> GetBookAsync(string name)
+            => await httpClient.GetFromJsonAsync<BookModel>($"data/books/{name}/book.json?{DateTime.Now:yyyyMMddHHmmss}");
+
+        public async Task<string> GetChapterDataAsync(string name, int chapter)
+            => await (await httpClient.GetAsync($"/data/books/{name}/{chapter:000}/sc01.xml?{DateTime.Now:yyyyMMddHHmmss}")).Content.ReadAsStringAsync();
+
+        public string GetBackgroundImageUrl(string imageName)
+            => $"/data/assets/background/{imageName}.jpg";
+
+        public async Task<Stream> GetStoryboardStreamAsync(string book, int chapter)
+            => await (await httpClient.GetAsync($"/data/books/{book}/{chapter:000}/sc01.xml?{DateTime.Now:yyyyMMddHHmmss}")).Content.ReadAsStreamAsync();
+
+        public async Task<Stream> GetThrophiesStreamAsync(string book, int chapter)
+            => await (await httpClient.GetAsync($"/data/books/{book}/{chapter:000}/trophies.xml?{DateTime.Now:yyyyMMddHHmmss}")).Content.ReadAsStreamAsync();
+    }
+}
---
+++ src/IS.Blazor/Shared/Services/IAssetManager.cs
@@ -0,0 +1,19 @@
+﻿using IS.Blazor.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace IS.Blazor.Services
+{
+    public interface IAssetManager
+    {
+        Task<BookModel> GetBookAsync(string name);
+        string GetBookCoverUrl(string bookName);
+        Task<IReadOnlyList<BookModel>> GetBooksAsync();
+        Task<string> GetChapterDataAsync(string name, int chapter);
+
+        string GetBackgroundImageUrl(string imageName);
+        Task<Stream> GetStoryboardStreamAsync(string book, int chapter);
+        Task<Stream> GetThrophiesStreamAsync(string book, int chapter);
+    }
+}
