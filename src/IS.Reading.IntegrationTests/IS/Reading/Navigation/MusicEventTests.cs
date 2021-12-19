013c9a9e2433e4d3365f13824a3706a558492421
Aldinei Sampaio <aldinei.sampaio@pgmais.com.br>
2021-12-19 07:43:40 -0300

    Teste integrado de eventos de música

---
+++ src/IS.Reading.IntegrationTests/IS/Reading/Navigation/MusicEventTests.cs
@@ -0,0 +1,26 @@
+﻿namespace IS.Reading.Navigation;
+
+public class MusicEventTests
+{
+    [Fact]
+    public async Task Music()
+    {
+        var xml =
+@"<storyboard>
+    <music>open_sky</music>
+    <pause />
+    <music>never_look_back</music>
+    <pause />
+</storyboard>";
+
+        var tester = await StoryboardEventTester.CreateAsync(xml);
+
+        await tester.ForwardAsync("music: open_sky");
+        await tester.ForwardAsync("music: never_look_back");
+        await tester.ForwardEndAsync("music undefined");
+
+        await tester.BackwardAsync("music: never_look_back");
+        await tester.BackwardAsync("music: open_sky");
+        await tester.BackwardEndAsync("music undefined");
+    }
+}
