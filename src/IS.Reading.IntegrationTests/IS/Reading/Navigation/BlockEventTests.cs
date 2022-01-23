de4db4b1dd1da67670988848c3891a69605db1e0
Aldinei Sampaio <aldinei.sampaio@pgmais.com.br>
2022-01-22 23:00:32 -0300

    Incluídos testes integrados para DoWhile :-))

--- src/IS.Reading.IntegrationTests/IS/Reading/Navigation/BlockEventTests.cs
+++ src/IS.Reading.IntegrationTests/IS/Reading/Navigation/BlockEventTests.cs
@@ -60,4 +60,93 @@ public class BlockEventTests
         await tester.BackwardAsync("tutorial: texto1");
         await tester.BackwardEndAsync("tutorial end");
     }
+
+    [Fact]
+    public async Task DoWhile_FalseCondition()
+    {
+        var xml =
+@"<storyboard>
+    <tutorial>texto1</tutorial>
+    <do while=""a=1"">
+        <narration>texto2</narration>
+    </do>
+    <tutorial>texto3</tutorial>
+</storyboard>";
+
+        var tester = await StoryboardEventTester.CreateAsync(xml);
+
+        await tester.ForwardAsync("tutorial start", "tutorial: texto1");
+        await tester.ForwardAsync("tutorial end", "tutorial start", "tutorial: texto3");
+        await tester.ForwardEndAsync("tutorial end");
+
+        await tester.BackwardAsync("tutorial start", "tutorial: texto3");
+        await tester.BackwardAsync("tutorial end", "tutorial start", "tutorial: texto1");
+        await tester.BackwardEndAsync("tutorial end");
+    }
+
+    [Fact]
+    public async Task DoWhile()
+    {
+        var xml =
+@"<storyboard>
+    <tutorial>texto1</tutorial>
+    <do while=""a!=3"">
+        <narration>texto2</narration>
+        <set>a++</set>
+    </do>
+    <tutorial>texto3</tutorial>
+</storyboard>";
+
+        var tester = await StoryboardEventTester.CreateAsync(xml);
+
+        await tester.ForwardAsync("tutorial start", "tutorial: texto1");
+        await tester.ForwardAsync("tutorial end", "narration start", "narration: texto2");
+        await tester.ForwardAsync("narration end", "narration start", "narration: texto2");
+        await tester.ForwardAsync("narration end", "narration start", "narration: texto2");
+        await tester.ForwardAsync("narration end", "tutorial start", "tutorial: texto3");
+        await tester.ForwardEndAsync("tutorial end");
+
+        await tester.BackwardAsync("tutorial start", "tutorial: texto3");
+        await tester.BackwardAsync("tutorial end", "narration start", "narration: texto2");
+        await tester.BackwardAsync("narration end", "narration start", "narration: texto2");
+        await tester.BackwardAsync("narration end", "narration start", "narration: texto2");
+        await tester.BackwardAsync("narration end", "tutorial start", "tutorial: texto1");
+        await tester.BackwardEndAsync("tutorial end");
+    }
+
+    [Fact]
+    public async Task DoWhile_Concatenated()
+    {
+        var xml =
+@"<storyboard>
+    <tutorial>texto1</tutorial>
+    <do while=""a!=2"">
+        <narration>texto2</narration>
+        <set>a++</set>
+        <do while=""b!=2"">
+            <narration>texto3</narration>
+            <set>b++</set>
+        </do>
+    </do>
+    <tutorial>texto4</tutorial>
+</storyboard>";
+
+        var tester = await StoryboardEventTester.CreateAsync(xml);
+
+        await tester.ForwardAsync("tutorial start", "tutorial: texto1");
+        await tester.ForwardAsync("tutorial end", "narration start", "narration: texto2");
+        await tester.ForwardAsync("narration end", "narration start", "narration: texto3");
+        await tester.ForwardAsync("narration end", "narration start", "narration: texto3");
+        await tester.ForwardAsync("narration end", "narration start", "narration: texto2");
+        await tester.ForwardAsync("narration end", "tutorial start", "tutorial: texto4");
+        await tester.ForwardEndAsync("tutorial end");
+
+        await tester.BackwardAsync("tutorial start", "tutorial: texto4");
+        await tester.BackwardAsync("tutorial end", "narration start", "narration: texto2");
+        await tester.BackwardAsync("narration end", "narration start", "narration: texto3");
+        await tester.BackwardAsync("narration end", "narration start", "narration: texto3");
+        await tester.BackwardAsync("narration end", "narration start", "narration: texto2");
+        await tester.BackwardAsync("narration end", "tutorial start", "tutorial: texto1");
+        await tester.BackwardEndAsync("tutorial end");
+    }
 }
